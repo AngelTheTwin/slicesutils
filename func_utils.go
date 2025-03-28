58d2c8a2884b108dd8167d0fa366ce95162f39b6
angelthetwin <angeldejesussanchezm@icloud.com>
2025-03-28 10:55:30 -0600

    [FEAT:add] added SafeExecuteWithStackTrace

--- func_utils.go
+++ func_utils.go
@@ -7,11 +7,11 @@ import (
 
 // SafeExcecute executes a given function and recovers from any panic that occurs during its execution.
 // It returns the output of the function and any error that occurred.
-// If a panic occurs, it captures the stack trace and returns it as an error.
+// If a panic occurs, it intercepts the panic and returns it as an error.
 func SafeExcecute[T_out any](fn func() (T_out, error)) (output T_out, err error) {
 	defer func() {
 		if r := recover(); r != nil {
-			err = getErrWithStackTrace()
+			err = r.(error)
 		}
 	}()
 
@@ -19,10 +19,24 @@ func SafeExcecute[T_out any](fn func() (T_out, error)) (output T_out, err error)
 	return
 }
 
-func getErrWithStackTrace() error {
+// SafeExcecuteWithStackTrace executes a function that returns a value and an error,
+// and ensures that any panic during the execution is recovered and converted into an error
+// with a stack trace.
+func SafeExcecuteWithStackTrace[T_out any](fn func() (T_out, error)) (output T_out, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(error)
+			err = fmt.Errorf("panic: %v\nStack trace:\n%s", err, getErrWithStackTrace())
+		}
+	}()
+
+	output, err = fn()
+	return
+}
+
+func getErrWithStackTrace() string {
 	buff := make([]byte, 4096)
 	n := runtime.Stack(buff, false)
 	stackTrace := string(buff[:n])
-	err := fmt.Errorf("encountered panic: \nStack trace:\n%s", stackTrace)
-	return err
+	return stackTrace
 }
